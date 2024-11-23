63ff3deb8fee35958eb503b11e83a629a8ff31c2
Vivekkumarloki <hp1030476@gmail.com>
2024-11-23 16:37:56 +0530

    Update start.py

--- BrandrdXMusic/plugins/bot/start.py
+++ BrandrdXMusic/plugins/bot/start.py
@@ -127,7 +127,7 @@ async def start_pm(client, message: Message, _):
             chat_photo = userss_photo
         chat_photo = userss_photo if userss_photo else START_IMG_URL
 
-        except AttributeError:
+    except AttributeError:
             chat_photo = "assets/nodp.png"
         await lols.delete()
         await m.delete()

b304819f2bfb92fd9470180df318c362ddb21419
Vivekkumarloki <hp1030476@gmail.com>
2024-11-23 17:01:02 +0530

    Update start.py

--- BrandrdXMusic/plugins/bot/start.py
+++ BrandrdXMusic/plugins/bot/start.py
@@ -84,54 +84,56 @@ async def start_pm(client, message: Message, _):
                     text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
                 )
     else:
-        out = private_panel(_)
-        lol = await message.reply_text("𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐁𝐚𝐛𝐲 ꨄ︎ {}.. ❣️".format(message.from_user.mention))
-        await lol.edit_text("𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐁𝐚𝐛𝐲 ꨄ {}.. 🥳".format(message.from_user.mention))
-        await lol.edit_text("𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐁𝐚𝐛𝐲 ꨄ {}.. 💥".format(message.from_user.mention))
-        await lol.edit_text("𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐁𝐚𝐛𝐲 ꨄ {}.. 🤩".format(message.from_user.mention))
-        await lol.edit_text("𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐁𝐚𝐛𝐲 ꨄ {}.. 💌".format(message.from_user.mention))
-        await lol.edit_text("𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐁𝐚𝐛𝐲 ꨄ {}.. 💞".format(message.from_user.mention))
+
+        try:
+            out = private_panel(_)
+            lol = await message.reply_text("𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐁𝐚𝐛𝐲 ꨄ︎ {}.. ❣️".format(message.from_user.mention))
+            await lol.edit_text("𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐁𝐚𝐛𝐲 ꨄ {}.. 🥳".format(message.from_user.mention))
+            await lol.edit_text("𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐁𝐚𝐛𝐲 ꨄ {}.. 💥".format(message.from_user.mention))
+            await lol.edit_text("𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐁𝐚𝐛𝐲 ꨄ {}.. 🤩".format(message.from_user.mention))
+            await lol.edit_text("𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐁𝐚𝐛𝐲 ꨄ {}.. 💌".format(message.from_user.mention))
+            await lol.edit_text("𝐖𝐞𝐥𝐜𝐨𝐦𝐞 𝐁𝐚𝐛𝐲 ꨄ {}.. 💞".format(message.from_user.mention))
                
-        await lol.delete()
-        lols = await message.reply_text("**⚡️ѕ**")
-        await asyncio.sleep(0.1)
-        await lols.edit_text("⚡ѕт")        
-        await asyncio.sleep(0.1)
-        await lols.edit_text("**⚡ѕтα**")
-        await asyncio.sleep(0.1)
-        await lols.edit_text("**⚡ѕтαя**")
-        await asyncio.sleep(0.1)
-        await lols.edit_text("**⚡ѕтαят**")
-        await asyncio.sleep(0.1)
-        await lols.edit_text("**⚡ѕтαятι**")
-        await asyncio.sleep(0.1)
-        await lols.edit_text("**⚡ѕтαятιи**")
-        await asyncio.sleep(0.1)
-        await lols.edit_text("**⚡ѕтαятιиg**")
-        await asyncio.sleep(0.1)
-        await lols.edit_text("**⚡ѕтαятιиg.**")
+            await lol.delete()
+            lols = await message.reply_text("**⚡️ѕ**")
+            await asyncio.sleep(0.1)
+            await lols.edit_text("⚡ѕт")        
+            await asyncio.sleep(0.1)
+            await lols.edit_text("**⚡ѕтα**")
+            await asyncio.sleep(0.1)
+            await lols.edit_text("**⚡ѕтαя**")
+            await asyncio.sleep(0.1)
+            await lols.edit_text("**⚡ѕтαят**")
+            await asyncio.sleep(0.1)
+            await lols.edit_text("**⚡ѕтαятι**")
+            await asyncio.sleep(0.1)
+            await lols.edit_text("**⚡ѕтαятιи**")
+            await asyncio.sleep(0.1)
+            await lols.edit_text("**⚡ѕтαятιиg**")
+            await asyncio.sleep(0.1)
+            await lols.edit_text("**⚡ѕтαятιиg.**")
 
-        await lols.edit_text("**⚡ѕтαятιиg....**")
+            await lols.edit_text("**⚡ѕтαятιиg....**")
 
-        await lols.edit_text("**⚡ѕтαятιиg.**")
-        await lols.edit_text("**⚡ѕтαятιиg....**")
-        m = await message.reply_sticker("CAACAgUAAxkBAAEQI1BlTLmx7PtOO3aPNshEU2gCy7iAFgACNQUAApqMuVeA6eJ50VbvmDME")
-        if message.chat.photo:
+            await lols.edit_text("**⚡ѕтαятιиg.**")
+            await lols.edit_text("**⚡ѕтαятιиg....**")
+            m = await message.reply_sticker("CAACAgUAAxkBAAEQI1BlTLmx7PtOO3aPNshEU2gCy7iAFgACNQUAApqMuVeA6eJ50VbvmDME")
+            if message.chat.photo:
 
-            userss_photo = await app.download_media(
-                message.chat.photo.big_file_id,
-            )
-        else:
-            userss_photo = "assets/nodp.png"
-        if userss_photo:
-            chat_photo = userss_photo
-        chat_photo = userss_photo if userss_photo else START_IMG_URL
+                userss_photo = await app.download_media(
+                    message.chat.photo.big_file_id,
+                )
+            else:
+                userss_photo = "assets/nodp.png"
+            if userss_photo:
+                chat_photo = userss_photo
+            chat_photo = userss_photo if userss_photo else START_IMG_URL
 
-    except AttributeError:
+        except AttributeError:
             chat_photo = "assets/nodp.png"
-    await lols.delete()
-    await m.delete()
-    await message.reply_photo(
+        await lols.delete()
+        await m.delete()
+        await message.reply_photo(
             photo=chat_photo,
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
